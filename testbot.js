84f2f24c05aa5a127fdc075863ec8d3cb3a51fa9
cesar-polanco <cesar.polanco@liferay.com>
2017-10-20 15:45:48 -0700

    Hackday: produce a thing

--- testbot.js
+++ testbot.js
@@ -1,8 +1,11 @@
+var request = require('request');
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-var bot_token = process.env.SLACK_BOT_TOKEN || <slack_token>;
+var bot_token = process.env.SLACK_BOT_TOKEN || '';
+
+const HEALTH_CHECK_LOCATION = "https://jenkinsci.wedeploy.io/counts"
 
 var rtm = new RtmClient(bot_token);
 
@@ -24,9 +27,90 @@ rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
 // New stuff
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   console.log('Message:', message); //this is no doubt the lamest possible message handler, but you get the idea
-  if(message.text === "Hello"){
-    rtm.sendMessage("Hello, <@" + message.user + ">!", message.channel);
+
+  if(checkIfForBot(message)){
+    // We know this is probably addressing Cid now.
+    // Check for the type of action to take
+    var output;
+    switch(checkForCommand(message)) {
+      case "jenkinsHealthCheck":
+        output = getJenkinsSlavesHealth(message);
+        break;
+      case "sayHello":
+        output = getHello(message);
+        break;
+      default:
+        output = getHelpMessage();
+    }
+
+    if (output !== undefined) {
+      rtm.sendMessage(output, message.channel);
+    } else {
+      rtm.sendMessage("Technical Issues, please stand by.", message.channel);
+    }
   }
+
 });
+
+function checkIfForBot(input) {
+  return input.text.toLowerCase().includes("cid");
+}
+
+function checkForCommand(message) {
+  var command = message.text.toLowerCase();
+  if (command.includes("jenkins slave status")) {
+    return "jenkinsHealthCheck";
+  } else if (command.includes("hello")) {
+    return "sayHello";
+  } else {
+    return "help";
+  }
+}
+
+function getHello(messObj) {
+  var result = "Hello <@" + messObj.user + ">!"
+  return result
+}
+
+function getHelpMessage() {
+  var result = "I didn't understand what you said\nHere are some of the " +
+              "things that I can do:\n----------------------\nJenkins Slave " +
+              "Status: Check on the " +
+              "health of the Jenkins Cluster\n\n Or you can always just " +
+              "say 'Hello!'";
+  return result;
+}
+
+function getJenkinsSlavesHealth(message) {
+  request(HEALTH_CHECK_LOCATION, (err, res, body) => {
+    var result;
+    if (res.statusCode === 200 && !err) {
+      let slaveStatusObject = JSON.parse(body);
+      console.log('Response: ', slaveStatusObject);
+      result = 'There are currently ' + slaveStatusObject['offline_slave_count'] +
+              ' slaves offline.';
+    } else {
+      result = "HTTP Error: " + res.statusCode + " occurred.";
+    }
+
+    rtm.sendMessage(result, message.channel);
+
+  });
+  return "One moment...";
+}
+
+function sendJenkinsSlavesHealth(error, response, body, channel) {
+  var result;
+  if (response.statusCode === 200 && !error) {
+    let slaveStatusObject = JSON.parse(body);
+    console.log('Response: ', slaveStatusObject);
+    result = 'There are currently ' + slaveStatusObject['offline_slave_count'] +
+            ' slaves offline.';
+  } else {
+    result = "HTTP Error: " + response.statusCode + " occurred.";
+  }
+
+  rtm.sendMessage(result, channel);
+}
 
 rtm.start();
