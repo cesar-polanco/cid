a93d92caf9ed33f9eaa9d9be3306b2c8d9bfaeac
cesar-polanco <cesar.polanco@liferay.com>
2017-10-20 10:35:10 -0700

    Initial commit, no readme

---
+++ testbot.js
@@ -0,0 +1,32 @@
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+
+var bot_token = process.env.SLACK_BOT_TOKEN || 'xoxb-245561467699-wgE4pDUComXkFCw6YbHZHAoJ';
+
+var rtm = new RtmClient(bot_token);
+
+let channel;
+
+// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+  for (const c of rtmStartData.channels) {
+	  if (c.is_member && c.name ==='general') { channel = c.id }
+  }
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+});
+
+// you need to wait for the client to fully connect before you can send messages
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
+  rtm.sendMessage("Hello!", channel);
+});
+
+// New stuff
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+  console.log('Message:', message); //this is no doubt the lamest possible message handler, but you get the idea
+  if(message.text === "Hello"){
+    rtm.sendMessage("Hello, <@" + message.user + ">!", message.channel);
+  }
+});
+
+rtm.start();
