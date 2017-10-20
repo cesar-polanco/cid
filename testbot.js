e85c1a9bca7b8740c833251f19dd1d8119f2c16f
cesar-polanco <cesar.polanco@liferay.com>
2017-10-20 11:34:28 -0700

    Woops

--- testbot.js
+++ testbot.js
@@ -2,7 +2,7 @@ var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-var bot_token = process.env.SLACK_BOT_TOKEN || 'xoxb-245561467699-wgE4pDUComXkFCw6YbHZHAoJ';
+var bot_token = process.env.SLACK_BOT_TOKEN || <slack_token>;
 
 var rtm = new RtmClient(bot_token);
 
@@ -11,7 +11,7 @@ let channel;
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   for (const c of rtmStartData.channels) {
-	  if (c.is_member && c.name ==='general') { channel = c.id }
+	  if (c.is_member && (c.name ==='general' || c.name === 'random')) { channel = c.id }
   }
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
